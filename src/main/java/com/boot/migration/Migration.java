e5449a6c3fe94e0b14f5c76a29f50bbbbd44a1ed
skokane <sandeep.kokane@questionpro.com>
2024-04-24 08:43:22 -0500

    added category and question id to qp questions

--- src/main/java/com/boot/migration/Migration.java
+++ src/main/java/com/boot/migration/Migration.java
@@ -1,5 +1,6 @@
 package com.boot.migration;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -7,10 +8,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class Migration implements ApplicationRunner {
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         log.info("test");
+        // get userinterlinks
+        // create panel_member, cx_transaction, responset_set, custom fields
+        //
     }
 
 
--- src/main/java/com/boot/questionpro/entity/Question.java
+++ src/main/java/com/boot/questionpro/entity/Question.java
@@ -20,9 +20,15 @@ public class Question {
 
     @Id
     private Integer id;
+    @Column(name = "survey_id")
     private Integer surveyId;
+    @Column(name = "q_text")
     private String qText;
     private String code;
+    @Column(name = "category_id")
+    private Integer categoryId;
+    @Column(name = "question_id")
+    private Integer questionId;
 
     @OneToMany(mappedBy = "q_id", fetch = FetchType.EAGER)
     private List<Answer> answersList = new LinkedList<>();
